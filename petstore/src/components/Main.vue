01bcc384aa79b774e3af39228526d9a631f0bebc
lcs <cslee@itmoulds.com>
2020-12-18 09:21:20 +0900

    Added - 8장 튜토리얼 - 애니메이션, 트래지션 추가하기

--- petstore/src/components/Main.vue
+++ petstore/src/components/Main.vue
@@ -26,17 +26,20 @@
             <button disabled="true" class="btn btn-primary btn-lg"
                     v-else>장바구니 담기
             </button>
-            <span class="inventory-message"
-                  v-if="product.availableInventory - cartCount(product) === 0">
-              품절!
-          </span>
-            <span class="inventory-message"
-                  v-else-if="product.availableInventory - cartCount(product) < 5">
-              {{ product.availableInventory - cartCount(product) }} 남았습니다!
-          </span>
-            <span class="inventory-message"
-                  v-else>지금 구매하세요!
-          </span>
+            <transition name="bounce" mode="out-in">
+              <span class="inventory-message"
+                    v-if="product.availableInventory - cartCount(product) === 0" key="0">
+                품절!
+              </span>
+                  <span class="inventory-message"
+                        v-else-if="product.availableInventory - cartCount(product) < 5" key="">
+                  {{ product.availableInventory - cartCount(product) }} 남았습니다!
+              </span>
+                  <span class="inventory-message" key=""
+                        v-else>지금 구매하세요!
+              </span>
+            </transition>
+
             <div class="rating">
             <span v-bind:class="{'rating-active': checkRating(n, product)}"
                   v-for="n in 5">☆
@@ -126,7 +129,7 @@ export default {
       }
     }
   },
-  created(){
+  created() {
     axios.get('/static/products.json').then(response => {
       this.products = response.data.products;
       console.log(this.products);
@@ -136,5 +139,25 @@ export default {
 </script>
 
 <style scoped>
+.bounce-enter-active {
+  animation: shake .72s cubic-bezier(.37, .07, .19, .97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+}
 
+@keyframes shake {
+  10%, 90% {
+    color: red;
+    transform: translate3d(-1px, 0, 0);
+  }
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
 </style>
